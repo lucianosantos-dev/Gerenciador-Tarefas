fd861c06e5e3bbd04cc535e1a8d2760a1f63f8e3
Luciano Santos <lucianodev.contato@gmail.com>
2025-09-14 00:39:53 -0300

    Criando classe TarefaController

---
+++ src/main/java/com/lucianodev/gerenciador_de_tarefas/controllers/TarefaController.java
@@ -0,0 +1,9 @@
+package com.lucianodev.gerenciador_de_tarefas.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/tarefas")
+public class TarefaController {
+}
